01443ace9ead4f7c1b64a2ec340e9fbf907917d2
Antoine Guerette <antoine4.guerette@gmail.com>
2020-10-06 09:13:04 -0400

    Ajout du CSS.

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
 
     //Un commentaire
     console.log("Ça fonctionne!!!");
 
+    //var hamburgers = document.querySelectorAll(".hamburger");
+    var hamburger = document.querySelector(".hamburger");
+
+    var menuMobile = document.querySelector(".nav-primary-mobile");
+
+    /*for (var i = 0; i < hamburgers.length; i++) {
+        var hamburger = hamburgers[i];
+        hamburger.addEventListener("click", openMenu);
+    }*/
+    hamburger.addEventListener("click", openMenu);
+
+
+    function openMenu(evt) {
+        evt.preventDefault();
+
+        var cible = evt.currentTarget;
+
+        if (cible.classList.contains("open")) {
+            cible.classList.remove("open");
+            menuMobile.classList.remove("open");
+        } else {
+            cible.classList.add("open");
+            menuMobile.classList.add("open");
+        }
+    }
 });
